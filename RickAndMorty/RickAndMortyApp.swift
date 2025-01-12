e9d6ce86b7723cd5a5dad411f5f879ab7e2c047a
hamza <hamza.jerbi@panel.es>
2025-01-12 12:24:36 +0100

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by HAMZA JERBI on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
